cf1e870f2ba8d8a13f768a7bffcfcb442a74be0b
JediMarty <130549056+JediMarty@users.noreply.github.com>
2024-08-13 10:45:25 +0300

    Update RestaurantMain.java

--- Restaurant/src/pack/RestaurantMain.java
+++ Restaurant/src/pack/RestaurantMain.java
@@ -7,7 +7,6 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,9 +17,15 @@ import javax.swing.JTextField;
 public class RestaurantMain {
 	
 	JFrame frame = new JFrame();
-	ImageIcon img_food;
+	ImageIcon img_food1;
 	ImageIcon img_food2;
+	ImageIcon img_drinks1;
+	ImageIcon img_drinks2;
+	ImageIcon img_waiter1;
+	ImageIcon img_waiter2;
 	JButton FoodIcon;
+	JButton DrinkIcon;
+	JButton WaiterIcon;
 	RestaurantMain() {
 		init();
 	}
@@ -34,10 +39,16 @@ public class RestaurantMain {
 	JLabel menuframe = new JLabel();
 	JTextField username = new JTextField();
 	JTextField password = new JTextField();
-	img_food = new ImageIcon("food.png");
+	img_food1 = new ImageIcon("food.png");
 	img_food2 = new ImageIcon("food2.png");
+	img_drinks1 = new ImageIcon("drinks1.png");
+	img_drinks2= new ImageIcon("drinks2.png");
+	img_waiter1 = new ImageIcon("waiter1.png");
+    img_waiter2 = new ImageIcon("waiter2.png");
 	ImageIcon menufr = new ImageIcon("menuframe.png");
 	FoodIcon = new JButton();
+	DrinkIcon = new JButton();
+	WaiterIcon = new JButton();
 	Font font = new Font("Calibri", Font.PLAIN , 30);
 	//...............................//
 	
@@ -61,14 +72,27 @@ public class RestaurantMain {
 	menuframe.setIcon(menufr);
 	menuframe.setBounds(500,0,500,100);
 	
-	FoodIcon.setIcon(img_food);
+	FoodIcon.setIcon(img_food1);
 	FoodIcon.setBorderPainted(false);
-	FoodIcon.setBounds(550, 15, 70, 70);
+	FoodIcon.setBounds(590, 15, 70, 70);
 	FoodIcon.setContentAreaFilled(false);
 	FoodIcon.setFocusPainted(false);
-	FoodIcon.setOpaque(false);
 	FoodIcon.addMouseListener(click_FoodIcon);
 	FoodIcon.setCursor(new Cursor(Cursor.HAND_CURSOR));
+	
+	DrinkIcon.setIcon(img_drinks1);
+	DrinkIcon.setBorderPainted(false);
+	DrinkIcon.setBounds(720, 15, 70, 70);
+	DrinkIcon.setContentAreaFilled(false);
+	DrinkIcon.addMouseListener(click_DrinkIcon);
+	DrinkIcon.setCursor(new Cursor(Cursor.HAND_CURSOR));
+	
+	WaiterIcon.setIcon(img_waiter1);
+	WaiterIcon.setBorderPainted(false);
+	WaiterIcon.setBounds(850, 15, 70, 70);
+	WaiterIcon.setContentAreaFilled(false);
+	WaiterIcon.addMouseListener(click_WaiterIcon);
+	WaiterIcon.setCursor(new Cursor(Cursor.HAND_CURSOR));
 	
 	//The Window
     frame.setTitle("Table Reserved");
@@ -77,6 +101,8 @@ public class RestaurantMain {
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
 	frame.setLayout(null);
 	frame.getContentPane().setBackground(Color.WHITE);
+	frame.add(WaiterIcon);
+	frame.add(DrinkIcon);
 	frame.add(FoodIcon);
 	frame.add(menuframe);
 	frame.add(panel1);
@@ -117,11 +143,78 @@ public class RestaurantMain {
 
 		@Override
 		public void mouseExited(MouseEvent e) {
-			FoodIcon.setIcon(img_food);
+			FoodIcon.setIcon(img_food1);
 			
 		}
 	
 	};
+	
+	MouseListener click_DrinkIcon = new MouseListener() {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			DrinkIcon.setIcon(img_drinks2);
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			DrinkIcon.setIcon(img_drinks1);
+			
+		}
+		
+	};
+	
+	MouseListener click_WaiterIcon = new MouseListener() {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			WaiterIcon.setIcon(img_waiter2);
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			WaiterIcon.setIcon(img_waiter1);
+			
+		}
+	
+	};
+
 }
-
-
